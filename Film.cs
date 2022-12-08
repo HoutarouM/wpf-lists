82249d388c2b1f5032926db7f039a3b8817b5bf6
HoutarouM <marian.horoshko@gmail.com>
2022-12-08 11:34:02 +0100

    lists

---
+++ Film.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace liste
+{
+    public class Film
+    {
+        // internal - dostepne w ramach paketow
+
+        public string Title { get; set; }
+
+        public int Score { get; set; }
+
+        public string Description { get; set; }
+
+        public string Genre { get; set; }
+
+        public bool Watched { get; set; }
+
+        public Film(string title, int score, string description, string genre)
+        {
+            Title = title;
+            Score = score;
+            Description = description;
+            Genre = genre;
+            Watched = false;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,9 @@ namespace liste
     public partial class MainWindow : Window
     {
         public IEnumerable<string> StringCollection { get; set; }
+        public ObservableCollection<Film> Films { get; set; }
+
+        public Film CheckedFilm { get; set; }
 
         public MainWindow()
         {
@@ -28,6 +32,25 @@ namespace liste
 
             StringCollection = "Ala ma kota Ala ma kota Ala ma kota Ala ma kota Ala ma kota Ala ma kota Ala ma kota Ala ma kota".Split(' ').ToList();
             DataContext = this;
+
+            prepareFilms();
+
+            CheckedFilm = Films[0];
+        }
+
+        private void prepareFilms()
+        {
+            Films = new ObservableCollection<Film>();
+
+            Films.Add(new Film("Vaiana", 6, "film", "animation"));
+            Films.Add(new Film("Kraina Lodu", 4, "film", "animation"));
+            Films.Add(new Film("Toys story", 6, "film", "animation"));
+            Films.Add(new Film("Avatar", 6, "film", "sci-fi"));
+        }
+
+        public void Show(object s, RoutedEventArgs e)
+        {
+            MessageBox.Show(CheckedFilm.Title);
         }
     }
 }
